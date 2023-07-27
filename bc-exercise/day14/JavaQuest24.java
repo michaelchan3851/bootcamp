0eef66b51a57d2f9cce4d9eec0271ef948c8ae0b
michaelchan3851 <michaelchan3851@gmail.com>
2023-07-27 18:09:13 +0800

    day13

--- bc-exercise/day14/JavaQuest24.java
+++ bc-exercise/day14/JavaQuest24.java
@@ -21,9 +21,9 @@ public class JavaQuest24 {
 
   public static int absolute(int[] nums, int target) {
     int count = 0;
-    for(int i = 0; i< nums.length;i++){
-      for(int j = 0; j<nums.length;j++){
-        if(Math.abs(target) == nums[i] - nums[j]){
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < nums.length; j++) {
+        if (nums[j] > nums[i] && target == Math.abs(nums[i] - nums[j])) {
           count++;
         }
       }
@@ -31,3 +31,4 @@ public class JavaQuest24 {
     return count;
   }
 }
+    
